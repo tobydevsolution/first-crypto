f461c6810e545fb5c1a311106969eac255898465
Tobiloba Orekoya <tobilobaorekoya19@gmail.com>
2025-07-24 20:10:01 +0100

    setting variable states

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className="  text-red-600 text-5xl">
+        Tobiloba Orekoya Explorer App{" "}
+      </h1>
+    </div>
+  );
+};
+
+export default App;
